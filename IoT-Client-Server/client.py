bdf16628fcd6ac22ad13a1add4c98b121ca4de58
Jobling <tiago.jobling@gmail.com>
2017-01-02 23:43:41 +0000

    Finished connection. Trying on mininet.

--- IoT-Client-Server/client.py
+++ IoT-Client-Server/client.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 # Imports
+import argparse
 import socket
 import sys
 
 def main():
+    parser = argparse.ArgumentParser(prog='IoT Client')
+    parser.add_argument('-i', '--ip', default='127.0.0.1', help='IoT server remote ip address.')
+    parser.add_argument('-p', '--port', type=int, default=9000, help='IoT server remote port.')
+    args = parser.parse_args()
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_ip = socket.gethostbyname(args.ip)
+    server_port = args.port
+    server_address = (server_ip, server_port)
+    
+    try:
+        s.connect(server_address)
+	message = s.recv(256)
+	print message
+    finally:
+        s.close()
+        
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
--- IoT-Client-Server/server.py
+++ IoT-Client-Server/server.py
@@ -3,6 +3,8 @@
 import socket
 import sys
 
+HOST = ''
+
 def get_host():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -13,14 +15,14 @@ def get_host():
     finally:
         s.close()
 
-def get_port():
+def get_port(host):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port = 9000
     while port < 10000:
         try:
-            s.bind((HOST, port))
+            s.bind((host, port))
             s.close()
-            return str(port)
+            return port
         except:
             port += 1
             pass    
@@ -28,24 +30,28 @@ def get_port():
     sys.exit(1)
 
 def get_address():
-    hostname = get_host()
-    port = get_port():
+    global HOST
+    hostname = ''
+    HOST = get_host()
+    port = get_port(hostname)
         
     return (hostname, port)
-        
+    
 def main():
     # Get ip address and port
     server_address = get_address()
     
     # Create socket and bind address
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print "Binding address %s:%s", server_address
+    print "Binding (external) address %s:%d" % (HOST, server_address[1])
     s.bind(server_address)
     
     s.listen(1)
     while(True):
         c, client_address = s.accept()
-        print client_address
+        message = "You, %s:%d, are connected to me, %s:%d. Welcome." % \
+		(client_address[0], client_address[1], HOST, server_address[1])
+        c.send(message)
         c.close()
         s.close()
         sys.exit(0)
